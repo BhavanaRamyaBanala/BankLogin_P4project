d093a39604ffa42afcbc5ec2e6fd2c72b7fd2d72
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-25 14:30:12 +0530

    First Commit

---
+++ Webapp_BankLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Webapp_BankLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
